486a8dacf4dbdb7db7ce2530513a4c775c249386
Mulla Ayeesha <ayeeshamulla0708@gmailcom>
2023-09-09 21:27:35 +0530

    Using if condition

---
+++ 02-print.sh
@@ -0,0 +1,6 @@
+echo -e "\e[31mred colour\e[0m"
+echo -e "\e[32mgreen colour\e[0m"
+echo -e "\e[33myellow colour\e[0m"
+echo -e "\e[34mblue colour\e[0m"
+echo -e "\e[35mmagenta colour\e[0m"
+echo -e "\e[36mcyan colour\e[0m"
