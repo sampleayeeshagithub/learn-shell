09156379f4dd66c351d558d435c9814d0365cc6f
Mulla Ayeesha <ayeeshamulla0708@gmailcom>
2023-09-09 23:35:49 +0530

    Using if condition

--- 05-special variables.sh
+++ 05-special variables.sh
@@ -1,7 +1,7 @@
 
-echo $0
-echo $1
-echo $2
-echo $@
-echo $*
-echo $#
+echo script name - $0
+echo first argument - $1
+echo second argument - $2
+echo all arguments - $@
+echo all arguments - $*
+echo no of arguments - $#
