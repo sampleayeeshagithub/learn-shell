ac84052eefa624b50ae21b0409f8069f942a5490
Mulla Ayeesha <ayeeshamulla0708@gmailcom>
2023-09-13 20:38:06 +0530

    Ansible

--- 07-mem-cal.sh
+++ 07-mem-cal.sh
@@ -4,3 +4,10 @@ free=$(free  |grep Mem | awk '{print $4}')
 used_percentage=$(echo "100 - ($free / $total * 100)" | bc -l | awk -F . '{print $1}')
 echo $used_percentage
 
+if [ $used_percentage -lt 50 ]; then
+  echo Mem stat is GREEN
+elif [ $used_percentage -lt 70 ]; then
+  echo Mem stat is ORANGE
+else
+  echo Mem stat is RED
+fi
