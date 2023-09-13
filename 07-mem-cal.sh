5662f8737f5bd006a44a48338bc578dd07b08325
Mulla Ayeesha <ayeeshamulla0708@gmailcom>
2023-09-13 20:18:35 +0530

    Ansible

--- 07-mem-cal.sh
+++ 07-mem-cal.sh
@@ -1,6 +1,6 @@
 total=$(free  | grep Mem | awk '{print $2}')
 free=$(free  |grep Mem | awk '{print $4}')
 
-used_percentage=$(echo "100 - ($free / $total * 100)" | bc -l  | awk -f . '{print $1}')
+used_percentage=$(echo "100 - ($free / $total * 100)" | bc -l | awk -F . '{print $1}')
 echo $used_percentage
 
